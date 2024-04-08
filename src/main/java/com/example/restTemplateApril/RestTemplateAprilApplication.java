4e2297d2b4a600b352aa37b0e4493ee2af2a2093
Kunal Jindal <kunaljindal@Kunals-MacBook-Pro.local>
2024-04-08 23:30:37 +0530

    Add Rest Template code

---
+++ src/main/java/com/example/restTemplateApril/RestTemplateAprilApplication.java
@@ -0,0 +1,13 @@
+package com.example.restTemplateApril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateAprilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateAprilApplication.class, args);
+	}
+
+}
